61858a781da19991c8fa64c4d9380ae79c6eb887
Yi Feng <fengyible@gmail.com>
2022-12-06 15:39:46 -0500

    fix: add loading state to fix wrong path caused by extra click

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Breadcrumb from './components/Breadcrumb/index';
 import FilesPanel from './components/FilesPanel/index';
+import { ReactComponent as LoadingLogo } from "./loading.svg";
 import './App.css';
 
 function App() {
 
   const [childrenFile, setChildrenFile] = useState({})
+  const [loading, setLoading] = useState(false);
   const [path, setPath] = useState([]);
 
   const handleClickBreadcrumb = (str) => {
@@ -16,16 +18,21 @@ function App() {
   const handlePathChange = async (pathsArr) => {
     const url = `http://127.0.0.1/path/${pathsArr.join("_")}`
     const last = pathsArr[pathsArr.length - 1]
+    setLoading(true);
     await fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setChildrenFile(data[last].children || {});
+        setLoading(false);
       });
   }
 
 
   const handleFileClick = (str) => {
-    setPath((data) => { return [...data, str] })
+    if (!loading) {
+      setPath((data) => { return [...data, str] })
+    }
+
   }
 
   useEffect(() => {
@@ -40,8 +47,10 @@ function App() {
 
   return (
     <div className="container">
-      <Breadcrumb paths={path} handlePathChange={handleClickBreadcrumb} />
-      <FilesPanel childrenFile={childrenFile} currentFolder={path[path.length - 1]} handleFileChange={handleFileClick} />
+      {loading && <div className="loadingMask"><LoadingLogo className="loadingLogo"
+        style={{ width: 40, height: 40 }} /></div>}
+      <Breadcrumb paths={path} loading={loading} handlePathChange={handleClickBreadcrumb} />
+      <FilesPanel childrenFile={childrenFile} loading={loading} currentFolder={path[path.length - 1]} handleFileChange={handleFileClick} />
     </div>
 
   );
